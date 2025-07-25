d212413fffe059ea692ed7dc8bc6f003c743c4d8
Alex Arno <lex.arno@gmail.com>
2025-07-24 22:29:41 -0300

    Adiciona NFSeServiceProvider compatível com Laravel

---
+++ src/Laravel/NFSe/NFSeServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laravel\NFSe;
+
+use Illuminate\Support\ServiceProvider;
+
+class NFSeServiceProvider extends ServiceProvider
+{
+  public function register(): void
+  {
+    //
+  }
+
+  public function boot(): void
+  {
+    //
+  }
+}

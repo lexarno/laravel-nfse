91affd35e2ba2dd6963ce3004b63c82fa6c974e1
Alex Arno <lex.arno@gmail.com>
2025-07-28 13:40:37 -0300

    atualizacoes

--- src/Laravel/NFSe/NFSeServiceProvider.php
+++ src/Laravel/NFSe/NFSeServiceProvider.php
@@ -9,14 +9,14 @@ class NFSeServiceProvider extends ServiceProvider
   public function register(): void
   {
     // Mescla o config com valores padrão, se o usuário não publicar
-    $this->mergeConfigFrom(__DIR__ . '/../config/nfse.php', 'nfse');
+    $this->mergeConfigFrom(__DIR__ . '/../../config/nfse.php', 'nfse');
   }
 
   public function boot(): void
   {
     // Publica o arquivo config/nfse.php no config do app Laravel
     $this->publishes([
-      __DIR__ . '/../config/nfse.php' => config_path('nfse.php'),
+      __DIR__ . '/../../config/nfse.php' => config_path('nfse.php'),
     ], 'nfse-config');
   }
 }

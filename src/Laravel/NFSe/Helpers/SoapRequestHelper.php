c14b8695cdbf123cf4c1aabc4157f834dadb2a58
Alex Arno <lex.arno@gmail.com>
2025-07-29 22:56:44 -0300

    nova versao

--- src/Laravel/NFSe/Helpers/SoapRequestHelper.php
+++ src/Laravel/NFSe/Helpers/SoapRequestHelper.php
@@ -4,6 +4,16 @@ namespace Laravel\NFSe\Helpers;
 
 class SoapRequestHelper
 {
+  /**
+   * Envia uma requisição SOAP para o provedor NFSe.
+   *
+   * @param string $url URL do endpoint SOAP
+   * @param string $operation Nome da operação SOAP (ex: RecepcionarLoteRps)
+   * @param string $cabecalhoXml Cabeçalho no padrão Abrasf (CDATA)
+   * @param string $xmlEnviado XML da requisição (CDATA)
+   * @return string XML de resposta do provedor
+   * @throws \Exception em caso de falha na requisição
+   */
   public static function enviar(string $url, string $operation, string $cabecalhoXml, string $xmlEnviado): string
   {
     $xml = <<<XML
@@ -21,7 +31,7 @@ XML;
     $headers = [
       'Content-Type: text/xml; charset=utf-8',
       'Content-Length: ' . strlen($xml),
-      "SOAPAction: \"$operation\""
+      "SOAPAction: \"$operation\"",
     ];
 
     $ch = curl_init($url);
@@ -29,15 +39,22 @@ XML;
     curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 
-    $resposta = curl_exec($ch);
+    $response = curl_exec($ch);
 
     if (curl_errno($ch)) {
       throw new \Exception('Erro ao enviar SOAP: ' . curl_error($ch));
     }
 
+    $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($statusCode !== 200) {
+      throw new \Exception("Erro HTTP $statusCode: $response");
+    }
+
     curl_close($ch);
 
-    return $resposta;
+    return $response;
   }
 }

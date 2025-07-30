f1aadd7429b63bed248b9fa2b7c03ee631b6deb9
Alex Arno <lex.arno@gmail.com>
2025-07-29 21:54:10 -0300

    nova versao

--- src/Laravel/NFSe/Helpers/XmlSigner.php
+++ src/Laravel/NFSe/Helpers/XmlSigner.php
@@ -18,6 +18,8 @@ class XmlSigner
     string $certPath,
     string $certPassword
   ): string {
+    $xmlContent = trim($xmlContent);
+
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->preserveWhiteSpace = false;
     $dom->formatOutput = false;
@@ -30,25 +32,24 @@ class XmlSigner
 
     $elementToSign = $elements->item(0);
 
-    // Cria a assinatura digital
     $objDSig = new XMLSecurityDSig();
     $objDSig->setCanonicalMethod(XMLSecurityDSig::EXC_C14N);
     $objDSig->addReference(
       $elementToSign,
-      XMLSecurityDSig::SHA1,
+      \RobRichards\XMLSecLibs\XMLSecurityDSig::SHA1,
       ['http://www.w3.org/2000/09/xmldsig#enveloped-signature'],
       ['id_name' => $attributeId, 'overwrite' => false]
     );
 
-    // Adiciona a chave privada
-    $objKey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA1, ['type' => 'private']);
+    $objKey = new XMLSecurityKey(
+      XMLSecurityKey::RSA_SHA1,
+      ['type' => 'private']
+    );
     $objKey->loadKey($certPath, true);
     $objDSig->sign($objKey);
 
-    // Adiciona a cadeia de certificados
     $objDSig->add509Cert(file_get_contents($certPath), true, false, ['subjectName' => true]);
 
-    // Insere a assinatura como filho do elemento assinado
     $objDSig->appendSignature($elementToSign);
 
     return $dom->saveXML();

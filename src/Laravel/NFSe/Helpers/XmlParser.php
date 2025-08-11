44cc43b4af6d925f2d3e87dba3ea1dd848db903d
Alex Arno <lex.arno@gmail.com>
2025-08-11 12:38:36 -0300

    ajustes

--- src/Laravel/NFSe/Helpers/XmlParser.php
+++ src/Laravel/NFSe/Helpers/XmlParser.php
@@ -1,31 +1,55 @@
 <?php
 
-namespace Laravel\NFSe\Helpers;
+namespace App\Helpers;
+
+use DOMDocument;
 
 class XmlParser
 {
-  protected \SimpleXMLElement $xml;
+  protected DOMDocument $dom;
 
   public function __construct(string $xml)
   {
-    // Remove namespace temporariamente para facilitar a leitura dos nós
-    $xml = preg_replace('/xmlns(:\w+)?="[^"]+"/', '', $xml);
-    $this->xml = simplexml_load_string($xml);
+    $normalized = $this->normalizeXml($xml);
+
+    $dom = new DOMDocument('1.0', 'UTF-8');
+    $dom->preserveWhiteSpace = false;
+    // Evita warnings e mantém CDATA como texto
+    $dom->loadXML($normalized, LIBXML_NOERROR | LIBXML_NOWARNING | LIBXML_NOCDATA);
+
+    $this->dom = $dom;
   }
 
+  /**
+   * Obtém o valor da primeira ocorrência de uma tag.
+   */
   public function get(string $tag): ?string
   {
-    // Busca profunda por todos os elementos com o nome da tag
-    $nodes = $this->xml->xpath("//*[local-name() = '{$tag}']");
-    if ($nodes && count($nodes) > 0) {
-      return (string) $nodes[0];
-    }
-
-    return null;
+    $nodes = $this->dom->getElementsByTagName($tag);
+    return $nodes->length > 0 ? trim($nodes->item(0)->textContent) : null;
   }
 
-  public function getRaw(): \SimpleXMLElement
+  /**
+   * Remove declaração XML, declarações xmlns e prefixos (ex: s:, ns2:, soap:)
+   */
+  protected function normalizeXml(string $xml): string
   {
-    return $this->xml;
+    // Remove BOM/espacos laterais
+    $xml = trim($xml);
+
+    // Remove declaração XML
+    $xml = preg_replace('/<\?xml[^>]*\?>/i', '', $xml);
+
+    // Remove TODAS as declarações de namespace (xmlns, xmlns:algo)
+    $xml = preg_replace('/\s+xmlns(:\w+)?="[^"]*"/i', '', $xml);
+
+    // Remove prefixos de namespace nas tags, ex: <s:Envelope> -> <Envelope>
+    // Funciona tanto para abertura quanto fechamento
+    $xml = preg_replace('/(<\/?)(\w+):/i', '$1', $xml);
+
+    // Opcional: compacta múltiplos espaços
+    $xml = preg_replace('/>\s+</', '><', $xml);
+
+    return $xml;
   }
 }
